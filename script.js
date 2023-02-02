f31802e8f3479b9b2b4cd53184bed7c78300b035
yusuf963 <mohammad963yusuf@gmail.com>
2023-02-01 23:41:00 -0800

    write caculation result function and clear function

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const input = document.getElementById("screen-display")
 // <input type="test" id="screen-display" value=''/>
 
-// input.value = 10 / 2
-
 //1.  we need to capture the first number
 //2.  we need to capture the operator 
 //3.  we need to capture the second number
+//4. we need function to caculate result
+//5. we need function tp clear the result after making the caculation
+
 let firstNumber = null
 let secondNumber = null
 let operator = null
@@ -25,5 +26,44 @@ function getOperator(sign){
      operator = sign
      console.log('its clicked and operator is ', operator)
     
+}
+
+function caculateResult(){
+    if(operator === '-'){
+        input.value = firstNumber - secondNumber
+    }
+    if(operator === '/'){
+        input.value = firstNumber / secondNumber
+    }
+    if(operator === '+'){
+        input.value = firstNumber + secondNumber
+    }
+    if(operator === '*'){
+        input.value = firstNumber * secondNumber
+    }
+
+    // switch (operator){
+    //     case '+':
+    //         input.value = firstNumber + secondNumber
+    //         break
+    //     case '/':
+    //         firstNumber / secondNumber
+    //         break
+    //     case '*':
+    //         firstNumber * secondNumber
+    //         break
+    //     case '-':
+    //         firstNumber - secondNumber
+    //         break
+    //     default:
+    //         console.log('please provide valid logical operator')
+    // }
+
+
+}
+
+function clearResult(){
+    input.value = null
+    location.reload()
 }
 
